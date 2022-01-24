7b83b579091ca6f7729d70eb2b889473fdac3f13
ovillalbah <oscarvillalba0807@gmail.com>
2022-01-24 15:14:52 -0500

    Manejadorbd

--- Codigo/manejadorbd.py
+++ Codigo/manejadorbd.py
@@ -7,10 +7,8 @@ from xml.dom import minicompat
 from validacion_datos import *
 from decorador import *
 
-class  ManejadorBD():
-        
-# Función para crear la bases de datos y la conexion con la base
-    def sql_conexion(self):
+
+def sql_conexion(self):
         try:
             con = sqlite3.connect('SpotyUN.db')
             return con
@@ -18,101 +16,98 @@ class  ManejadorBD():
         except Error:
             print(Error)
 
+class  ManejadorBD():
+    def __init__(self, conexionbd):
+        self.conexionbd = conexionbd.cursor()
+    
+
     # Función que elimina toda la información de una tabla
-    def eliminar_info_tablas(self, con, nombre_tabla: str):
-        cursor_obj = con.cursor()
-        cursor_obj.execute(f'DELETE from {self.nombre_tabla}')
-        con.commit()
+    def eliminar_info_tablas(self, nombre_tabla: str):
+        self.conexionbd.execute(f'DELETE from {nombre_tabla}')
+        self.conexionbd.commit()
 
 
-# Función que borrar la información de una tabla de acuerdo al primary_key sumistrado
-    def borrar_info(self, con, nombre_tabla: str, primary_key: str, id):
-        cursor = con.cursor()
-        self.id = id
-        borrar = f'DELETE FROM {self.nombre_tabla} WHERE {self.primary_key} = {id}'
-        cursor.execute(borrar)
-        con.commit()
-        print_line_success(f"Su registro a sido eliminado de la tabla {self.nombre_tabla} ;)")
+    # Función que borrar la información de una tabla de acuerdo al primary_key sumistrado
+    def borrar_info(self,nombre_tabla: str, primary_key: str):        
+        id = input("\nId de la información a eliminar: ")
+        borrar = f'DELETE FROM {nombre_tabla} WHERE {primary_key} = {id}'
+        self.conexionbd.execute(borrar)
+        self.conexionbd.commit()
+        print_line_success(f"Su registro a sido eliminado de la tabla {nombre_tabla} ;)")
 
 
-# Función para borrar cualquier tabla
-    def borrar(self, con, nombre_tabla):
-        cursorObj = con.cursor()
-        cursorObj.execute(f'DROP TABLE {self.nombre_tabla}')
-        con.commit()
+    # Función para borrar cualquier tabla
+        def borrar(self, nombre_tabla):
+            self.conexionbd.execute(f'DROP TABLE {nombre_tabla}')
+            self.conexionbd.commit()
 
 
-# Función para cerrar la base de datos
-    def close(con):
-        con.close()
+    # Función para cerrar la base de datos
+        def close(self):
+            self.conexionbd.close()
 
 
-# Función para crear la tabla 'canciones' en la base de datos
-def crear_tabla_canciones(con):
-    cursor_obj = con.cursor()
-    cursor_obj.execute("""CREATE TABLE IF NOT EXISTS canciones(
-                        id_cancion INTEGER PRIMARY KEY AUTOINCREMENT,
-                        nombre_cancion TEXT(100) NOT NULL,
-                        genero TEXT(30) NOT NULL,
-                        album  TEXT(100) NOT NULL,
-                        interprete TEXT(100) NOT NULL,
-                        imagen BLOB,
-                        cancion BLOB)""")
-    con.commit()
+    # Función para crear la tabla 'canciones' en la base de datos
+    def crear_tabla_canciones(self):
+        self.conexionbd.execute("""CREATE TABLE IF NOT EXISTS canciones(
+                            id_cancion INTEGER PRIMARY KEY AUTOINCREMENT,
+                            nombre_cancion TEXT(100) NOT NULL,
+                            genero TEXT(30) NOT NULL,
+                            album  TEXT(100) NOT NULL,
+                            interprete TEXT(100) NOT NULL,
+                            imagen BLOB,
+                            cancion BLOB)""")
+        self.conexionbd.commit()
 
 
-# Función para crear la tabla 'planes' en la base de datos
-def crear_tabla_planes(con):
-    cursor_obj = con.cursor()
-    cursor_obj.execute("""CREATE TABLE IF NOT EXISTS planes(
-                        id_plan INT(1) PRIMARY KEY UNIQUE,
-                        nombre_plan TEXT(15) NOT NULL,
-                        valor INT(5) NOT NULL,
-                        cantidad_canciones SHORT(4))""")
+    # Función para crear la tabla 'planes' en la base de datos
+    def crear_tabla_planes(self):
+        self.conexionbd.execute("""CREATE TABLE IF NOT EXISTS planes(
+                            id_plan INT(1) PRIMARY KEY UNIQUE,
+                            nombre_plan TEXT(15) NOT NULL,
+                            valor INT(5) NOT NULL,
+                            cantidad_canciones SHORT(4))""")
 
-    con.commit()
-
-
-# Función para crear la tabla 'clientes' en la base de datos
-def crear_tabla_clientes(con):
-    cursor_obj = con.cursor()
-    cursor_obj.execute("""CREATE TABLE IF NOT EXISTS clientes(
-                        id_cliente INTEGER(12) PRIMARY KEY UNIQUE,
-                        nombre_cliente TEXT(30) NOT NULL,
-                        apellido TEXT(30) NOT NULL,
-                        pais TEXT(30) NOT NULL,
-                        ciudad TEXT(30) NOT NULL,
-                        celular TEXT(15) NOT NULL,
-                        correo TEXT(45) NOT NULL,
-                        fecha_pago TEXT(10) NOT NULL,
-                        numero_tc TEXT(20) NOT NULL,
-                        estado_pago TEXT(15))""")
-
-    con.commit()
+        self.conexionbd.commit()
 
 
-# Función para crear la tabla 'listas' en la base de datos
-def crear_tabla_listas(con):
-    cursor_obj = con.cursor()
-    cursor_obj.execute("""CREATE TABLE IF NOT EXISTS listas(
-                        id_cancion INTEGER(10),
-                        nombre_cancion TEXT(100) NOT NULL,
-                        interprete TEXT(100) NOT NULL,
-                        album TEXT(100) NOT NULL,
-                        genero TEXT(30) NOT NULL,
-                        id_cliente INTEGER(12))""")
+    # Función para crear la tabla 'clientes' en la base de datos
+    def crear_tabla_clientes(self):
+        self.conexionbd.execute("""CREATE TABLE IF NOT EXISTS clientes(
+                            id_cliente INTEGER(12) PRIMARY KEY UNIQUE,
+                            nombre_cliente TEXT(30) NOT NULL,
+                            apellido TEXT(30) NOT NULL,
+                            pais TEXT(30) NOT NULL,
+                            ciudad TEXT(30) NOT NULL,
+                            celular TEXT(15) NOT NULL,
+                            correo TEXT(45) NOT NULL,
+                            fecha_pago TEXT(10) NOT NULL,
+                            numero_tc TEXT(20) NOT NULL,
+                            estado_pago TEXT(15))""")
 
-    con.commit()
+        self.conexionbd.commit()
 
 
-# Función para crear la tabla 'planes_cliente' en la base de datos
-def crear_tabla_planes_por_cliente(con):
-    cursor_obj = con.cursor()
-    cursor_obj.execute("""CREATE TABLE IF NOT EXISTS planes_cliente(
-                        id_cliente INTEGER(12) NOT NULL,
-                        id_plan INT(1) NOT NULL)""")
+    # Función para crear la tabla 'listas' en la base de datos
+    def crear_tabla_listas(self):
+        self.conexionbd.execute("""CREATE TABLE IF NOT EXISTS listas(
+                            id_cancion INTEGER(10),
+                            nombre_cancion TEXT(100) NOT NULL,
+                            interprete TEXT(100) NOT NULL,
+                            album TEXT(100) NOT NULL,
+                            genero TEXT(30) NOT NULL,
+                            id_cliente INTEGER(12))""")
 
-    con.commit()
+        self.conexionbd.commit()
+
+
+    # Función para crear la tabla 'planes_cliente' en la base de datos
+    def crear_tabla_planes_por_cliente(self):
+        self.conexionbd.execute("""CREATE TABLE IF NOT EXISTS planes_cliente(
+                            id_cliente INTEGER(12) NOT NULL,
+                            id_plan INT(1) NOT NULL)""")
+
+        self.conexionbd.commit()
 
 
 # Función para actualizar información de una tabla en la base de datos
