2cb6f333e230dda52acb4e68c0bb36d2eba569f7
ovillalbaUsa <91650236+ovillalbaUsa@users.noreply.github.com>
2022-01-31 11:54:15 -0500

    Oscar  listas_canciones

--- Codigo/controlador/base.py
+++ Codigo/controlador/base.py
@@ -12,6 +12,9 @@ from datetime import datetime
 import sqlite3
 from sqlite3 import Error
 from subprocess import HIGH_PRIORITY_CLASS
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 class Actualizar():
     def __init__(self):
@@ -779,6 +782,262 @@ class PP_cliente(Planes):
         self.con.commit()
         print_line_success('¡Su registro se ha realizado exitosamente!')
 
+    
+    
+class Listas_cliente(Actualizar):
+# Función para realizar la consulta de datos de la canción como id_canción, nombre_canción, interprete, album
+    def id_cancion_lista(self) -> list:
+        canciones = input('\nIngrese el id de cancion que desea agregar a su lista: ')
+        while canciones == '0':
+            break
+        cursor_obj = self.con.cursor()
+        cursor_obj.execute(f"SELECT * FROM canciones WHERE id_cancion = {canciones}")
+        filas = cursor_obj.fetchall()
+        for row in filas:
+            id_cancion = row[0]
+            nombre_cancion = row[1]
+            interprete = row[4]
+            album = row[3]
+            genero = row[2]
+        lista_info_cancion = [id_cancion, nombre_cancion, interprete, album, genero]
+        return lista_info_cancion
+
+
+# Función para consultar la cantidad de canciones por plan de acuerdo al registro del cliente
+    def plan_cliente(self, id_cliente: int) -> int:
+        cursor_obj = self.con.cursor()
+        cursor_obj.execute(f'SELECT id_plan FROM planes_cliente WHERE id_cliente = {id_cliente}')
+        id_plan_cliente = cursor_obj.fetchone()
+        cursor_obj.execute(f'SELECT cantidad_canciones FROM planes WHERE id_plan = {id_plan_cliente[0]}')
+        cant_canciones = cursor_obj.fetchone()
+        return cant_canciones
+
+
+# Función que reliza un conteo de los registros en la tabla listas de acuerdo al id_cliente suministrado
+    def contar_lista(self, id_cliente: int) -> str or int:
+        cursor_obj = self.con.cursor()
+        cursor_obj.execute(f'SELECT COUNT(*) FROM listas WHERE id_cliente = {id_cliente}')
+        cantidad_listas = cursor_obj.fetchone()
+        if cantidad_listas[0] == None:
+            return 0
+        return cantidad_listas
+
+
+# Función para consultar las canciones existentes en la tabla canciones y mostrarlas en pantalla
+    def consulta_tabla_canciones_lista(self):
+        cursor_obj = self.con.cursor()
+        cursor_obj.execute('SELECT id_cancion, nombre_cancion, genero, album, interprete  FROM canciones')
+        cantidad_canciones = cursor_obj.fetchall()
+        print("\n{:<12} {:<20} {:<20} {:<20} {:<20}".format('ID', 'NOMBRE', 'GENERO', 'ALBUM', 'INTERPRETE(S)'))
+        for row in cantidad_canciones:
+            id, nombre, genero, album, interprete = row
+            print("{:<12} {:<20} {:<20} {:<20} {:<20}".format(id, nombre, genero, album, interprete))
+
+
+# Función para agregar en una tupla la información correspondiente para la tabla listas
+# necesitamos el id_cliente y la lista info_canciones
+    def info_lista(self, id_cliente: int) -> tuple:
+        id_c = id_cliente
+        info_canciones = self.id_cancion_lista(self)
+        info_canciones.append(id_c)
+        return info_canciones
+
+
+# Función para registrar la información en la tabla listas
+    def registrar_lista_cliente(self, tupla: tuple):
+        cursor = self.con.cursor()
+        cursor.execute('''INSERT INTO listas VALUES (?,?,?,?,?,?)''', tupla)
+        self.con.commit()
+        print_line_success("¡¡El registro se ha realizado exitosamente!!")
+
+
+# Función para consultar la lista de canciones registradas en la tabla listas
+# de acuerdo al id suministrado por el cliente
+    def consulta_tabla_listas(self, id_cliente: int):
+        cursor_obj = self.con.cursor()
+        cursor_obj.execute(f'SELECT id_cancion, nombre_cancion, interprete, album, genero FROM listas WHERE id_cliente = {id_cliente}')
+        cantidad_canciones = cursor_obj.fetchall()
+        print("\n{:<12} {:<30} {:<20} {:<20} {:<20}".format('ID_CANCIÓN', 'NOMBRE', 'INTERPRETE', 'ALBUM', 'GENERO'))
+        for row in cantidad_canciones:
+            id, nombre, interprete, album, genero = row
+            print("{:<12} {:<30} {:<20} {:20} {:<20}".format(id, nombre, interprete, album, genero))
+
+
+# Función que borra una lista de reproducción de la tabla listas de acuerdo al id_cliente suministrado
+    def borrar_lista(self, id_cliente: int) -> str:
+        cursor_obj = self.con.cursor()
+        cursor_obj.execute(f'DELETE FROM listas WHERE id_cliente = {id_cliente}')
+        self.con.commit()
+        return print('Tu lista ha sido eliminada')
+
+
+# Función que consulta la información ingresa por el cliente en la tabla listas, retorna una tupla
+# una tupla de listas con la información completa de la lista de reproducción
+    def consulta_tabla_listas_id(self, id_cliente: int) -> bool or tuple:
+        cursor_obj = self.con.cursor()
+        cursor_obj.execute(f'SELECT id_cancion, nombre_cancion, interprete, album, genero FROM listas WHERE id_cliente = {id_cliente}')
+        cantidad_canciones = cursor_obj.fetchall()
+        if cantidad_canciones == []:
+            return False
+        else:
+            return cantidad_canciones
+
+
+# Función para realizar la consulta de la tabla listas para poder enviarla en el correo electrónico
+# Retorna un string con formato html para poderlo ingresar en la función enviar_correo
+    def consulta_tabla_para_html(self, id_cliente: int) -> list:
+        cursor_obj = self.con.cursor()
+        cursor_obj.execute(f'SELECT id_cancion, nombre_cancion, interprete, album, genero FROM listas WHERE id_cliente = {id_cliente}')
+        cantidad_canciones = cursor_obj.fetchall()
+        lista_str = ''
+        for row in cantidad_canciones:
+            id, nombre_cancion, interprete, album, genero = row
+            lista_str += (f"<tr><td>{id}</td><td>{nombre_cancion}</td><td>{interprete}</td><td>{album}</td><td>{genero}</td></tr>")
+        return lista_str
+
+
+# Función para enviar el mensaje con la lista de reproducción del cliente
+    def enviar_mensaje(self, id_cliente: int):
+        info_tabla = self.consulta_tabla_para_html(self, id_cliente)
+        canciones_plan= self.plan_cliente(self, id_cliente)[0]
+        canciones_lista = self.contar_lista(self, id_cliente)[0]
+        canciones_disponibles = canciones_plan - canciones_lista
+        mensaje = MIMEMultipart()
+        mensaje['Subject'] = 'CONFIRMACION DE LISTA DE REPRODUCCIÓN'
+        html = f"""\
+            <html>
+            <head></head>
+            <body>
+                <p>Hi! Spotynauta<br>
+                Envio tu lista de reproducción ;)               
+                <br>
+                <table class="default" border="2" "cellspacing="10" bordercolor="#ffffff">
+                    <tr>
+                    <th>Id canción</th><th>Nombre de canción</th><th>Interprete</th><th>Album</th><th>Género</th>
+                    </tr>            
+                    {info_tabla} 
+                </table>
+                <br>Información de tu plan:<br>
+                
+                <br>  Canciones totales del plan: {canciones_plan}<br>
+                <br>  Canciones totales en tu lista: {canciones_lista}<br>
+                    
+                <br> Canciones disponibles de tu plan: {canciones_disponibles}<br>
+                
+                </p>
+            </body>
+            </html>
+            """
+        mensaje.attach(MIMEText(html, 'html'))
+        texto = mensaje.as_string()
+        servidor = smtplib.SMTP('smtp.gmail.com', 587)
+        servidor.starttls()
+        correo = self.consulta_correo_cliente(self, id_cliente)
+        servidor.login('conectandotropadelta@gmail.com', 'conectandodelta')
+        servidor.sendmail('conectandotropadelta@gmail.com', correo, texto)
+        servidor.quit()
+        print_line_success("Envio exitoso")
+
+
+
+# Función para realizar la actualizacíón de un registro completo de la tabla listas
+    def actualizar_info_tabla_listas(self, tupla: tuple, id_cancion: int, id_cliente: int):
+        cursor_obj = self.con.cursor()
+        act_listas = (f"""UPDATE listas 
+                        SET id_cancion = ?,
+                            nombre_cancion = ?,
+                            interprete = ?,
+                            album = ?,
+                            genero = ?
+                        WHERE id_cancion = {id_cancion} and id_cliente = {id_cliente}""")
+        cursor_obj.execute(act_listas, tupla)
+        self.con.commit()
+
+
+# Función que despliega el menu de sección de listas de reproducción
+    def menu_lista(self, id_cliente: int):
+        state = True
+        while state:
+            print_line_menu("""
+                MENU SECCIÓN LISTAS DE REPRODUCCIÓN
+            1. Crear una lista
+            2. Consultar lista
+            3. Actualizar lista
+            4. Eliminar lista
+            5. Envia correo con lista de reproducción
+            6. Reproducir canciones
+            7. Ir al menu anterior""")
+        
+            opc = input('\nDigite una opción: ')
+            if opc == "1":
+                print("\nSesión de creación de lista de reproducción\n")
+                state_lista = True
+                while state_lista:
+                    try:
+                        # Se realiza el conteo de las canciones totales contratadas por el cliente de acuerdo al plan
+                        # y se realizar el conteo de las canciones que ya estan en la lista de reproducción.
+                        # asi tener el total de canciones faltantes para completar el plan
+                        total_canciones = self.plan_cliente(self, id_cliente)[0] - self.contar_lista(self, id_cliente)[0]
+                        print('Falta {} canciones para completar el plan'.format(total_canciones))
+                        if total_canciones == 0:
+                            print_line_error('¡Has completado tu plan, no puedes agregar más canciones!')
+                            state_lista = False
+                        else:
+                            self.consulta_tabla_canciones_lista(self)
+                            info = self.info_lista(self, id_cliente)
+                            self.registrar_lista_cliente(self, info)
+                            next = False
+                            while not next:
+                                opc_next = input("""\n¿Desea agregar otra canción? s/n: """).lower()
+                                if opc_next == 's':
+                                    next = True
+                                elif opc_next == 'n':
+                                    next = True
+                                    state_lista = False
+                                else:
+                                    print_line_error('¡Opción no valida. Digite una opción nuevamente!')
+                    except:
+                        print_line_error('¡Canción no encontrada en la base de datos!')
+            elif opc == "2":
+                self.consulta_tabla_listas(self, id_cliente)
+            elif opc == "3":
+                state_actualizar = True
+                while state_actualizar:
+                    try:
+                        self.consulta_tabla_listas(self, id_cliente)
+                        id_cancion = int(input('\nDigite el id de la canción que desea actualizar: '))
+                        self.consulta_tabla_canciones_lista(self)
+                        lista_info = self.info_lista(self, id_cliente)
+                        self.actualizar_info_tabla_listas(self, tuple(lista_info[0:5]), id_cancion, id_cliente)
+                        state_actualizar = False
+                    except:
+                        print_line_error('¡Canción no encontrada en las base de datos!')
+
+            elif opc == "4":
+                self.borrar_lista(self, id_cliente)
+            elif opc == "5":
+                self.enviar_mensaje(self, id_cliente)
+            elif opc == "6":
+                if self.contar_lista(self, id_cliente)[0] == 0:
+                    print_line_error('¡No tienes canciones en tu lista de reproducción!\nCrea una lista para poder reproducir canciones')
+                else:
+                    self.consulta_tabla_listas(self, id_cliente)
+                    id_cancion = int(input('\nDigite el id de la canción que desea reproducir: '))
+                    id_validacion = val.existencia_tablas(self, 'listas', 'id_cancion', 'id_cancion', id_cancion)
+                    while id_validacion != False:
+                        id_cancion = int(input('\nDigite el id de la canción que desea reproducir: '))
+                        id_validacion = val.existencia_tablas(self, 'listas', 'id_cancion', 'id_cancion', id_cancion)
+                    self.reproducir_cancion(self, id_cancion, id_cliente)
+            elif opc == "7":
+                state = False
+
+            else:
+                print("\t\n¡Opcion no valida. Digite una opción nuevamente!")
+
+"""----------------------------- Pruebas -----------------------------"""
+
+listas = Listas_cliente()
+listas.menu_lista(1)
     
 # ----------------------------------------------------------------------------
 
