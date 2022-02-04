9662b11778f276ff683ae777e08be275adb5e344
ovillalbah <oscarvillalba0807@gmail.com>
2022-02-03 19:57:46 -0500

    Version preparcial

--- Codigo/base.py
+++ Codigo/base.py
@@ -14,7 +14,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-class Actualizar():
+class M():
     def __init__(self):
         self.con = Actualizar.__sql_conexion(self)
 
